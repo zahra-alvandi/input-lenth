1d266a24bc3d1b83a3d4a4e0daeff87857a3f30a
Zahra Alvandi <zalvandi39@gmail.com>
2025-07-19 10:39:41 +0330

    update app.js

--- js/app.js
+++ js/app.js
@@ -2,6 +2,6 @@ const input = document.querySelector("input");
 const remaining = document.querySelector(".max-length");
 const inputMaxLength = +input.getAttribute("maxlength");
 
-input.addEventListener("keyup", function() {
-    console.log(input.value.length)
+input.addEventListener("keyup", function () {
+    remaining.innerHTML = inputMaxLength - input.value.length;
 })
